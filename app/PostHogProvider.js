0d7efb79c539685af6535e2805f69f7b6ac51db0
reponsesecurisee-pixel <reponsesecurisee@gmail.com>
2026-02-06 17:03:57 +0000

    Fix PostHog client initialization

--- app/PostHogProvider.js
+++ app/PostHogProvider.js
@@ -1,18 +1,22 @@
 'use client';
 
 import { useEffect } from 'react';
-import posthog from 'posthog-js';
 
 export default function PostHogProvider() {
   useEffect(() => {
-    posthog.init('phc_RdD2Quf2gsHL3NK1CdXwWMXBho4WqkFve6UbYXzBgtc', {
-      api_host: 'https://eu.posthog.com',
-      autocapture: false,
-    });
+    import('posthog-js').then((posthog) => {
+      posthog.default.init(
+        'phc_RdD2Quf2gsHL3NK1CdXwWMXBho4WqkFve6UbYXzBgtc',
+        {
+          api_host: 'https://eu.posthog.com',
+          autocapture: false,
+        }
+      );
 
-    if (localStorage.getItem('is_owner') === 'true') {
-      posthog.register({ is_owner: true });
-    }
+      if (localStorage.getItem('is_owner') === 'true') {
+        posthog.default.register({ is_owner: true });
+      }
+    });
   }, []);
 
   return null;
